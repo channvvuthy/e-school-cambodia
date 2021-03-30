de7dc5ec82d8331b2e61d4695afc979e15a20d06
channvuthy <channvuthyit@gmail.com>
2021-03-30 13:19:17 +0700

    V3.2.3

--- vue.config.js
+++ vue.config.js
@@ -9,9 +9,14 @@ module.exports = {
             builderOptions: {
                 "appId": "com.e-school-cambodia.com",
                 "productName": "E-SCHOOL",
+                "copyright":"Copyright ©2021 E-School Cambodia",
                 "publish":{
                     "provider":"github",
                     "owner":"channvuthy271",
+                    "host":"github.com",
+                    "protocol":"https",
+                    "token":"0be5711f8255e99370fc465ae1a6305ae811b10c",
+                   "releaseType":"release",
                 },
                 "win": {
                     "target": [
