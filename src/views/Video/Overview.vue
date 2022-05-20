f2d7cee7c1aa8341ec4f47adc884c8ede7830f1d
channvuthy <channvuthyit@gmail.com>
2022-05-20 14:32:29 +0700

    Update status 504

--- src/views/Video/Overview.vue
+++ src/views/Video/Overview.vue
@@ -1,125 +1,132 @@
 <template>
-    <div :class="darkMode ?`bg-youtube` : `bg-gray-50`" class="p-5 h-screen overflow-y-scroll">
-        <div class="w-9/12" :class="darkMode ?`bg-youtube text-textSecondary` : `bg-white shadow rounded-b-xl`">
-            <div class="relative">
-                <img :src="detail.thumbnail" class="w-full m-auto">
-                <div class="w-full absolute left-0 -bottom-5 flex justify-center">
-                    <button
-                            @click="gotToPlayList"
-                            class="flex items-center justify-center bg-primary text-white w-1/2 py-2 outline-none rounded-full shadow text-xl">
-                        <VideoCameraIcon fill="#fff" :size="35"/>
-                        <span class="ml-3">{{ $t('1117') }}</span>
-                    </button>
-                </div>
-            </div>
-            <div class="my-10 px-5">
-                <div class="font-bold text-2xl">{{ detail.teacher.name }} ({{ detail.title }})</div>
-                <div class="flex justify-between items-center my-5">
-                    <div class="text-lg">
-                        <template v-if="detail.price.highlight">
-                            {{ $t('1006') }}:
-                            <span class="mr-3">
-                $ <del>{{ khmerNumber(detail.price.highlight) }}</del>
-              </span>
-                            <span class="text-red-600 font-semibold">
-                $ {{ khmerNumber(detail.price.year) }}
-              </span>
-                        </template>
-                        <template v-else>
-                            <span>{{ $t('1007') }}</span>
-                        </template>
-                    </div>
+    <div>
+        <a
+                :class="darkMode ? `text-gray-300`: `text-primary`"
+                @click="()=>{this.$router.go(-1)}"
+                href="#" class="absolute right-2 bottom-2">
+            <BackMenuIcon :fill="darkMode?`#ffffff`:`#055174`" :width="30"/>
+        </a>
 
-                    <div class="flex items-center">
-                        <Eye :height="30"></Eye>
-                        <span v-if="loading">
-              <Loader :size="10" bg="#000"></Loader>
-            </span>
-                        <span v-else>
-              <span class="pl-2">
-              {{ kFormatter(playlist.course.view) }}
-            </span>
-            </span>
-                    </div>
-
-                    <div class="cursor-pointer">
-                        <div
-                                v-if="playlist.is_favorite"
-                                @click="removeFavorite"
-                        >
-                            <FavoriteFill
-                                    fill="#c0272d"
-                                    :size="30"/>
-                        </div>
-                        <div
-                                v-else
-                                @click="addToFavorite">
-                            <FavoriteIcon
-                                    :size="30"/>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="border-b border-t py-3 px-5" :class="darkMode ?`border-button` : ``" v-if="!loading">
-                <div class="flex items-center text-center justify-between w-96">
-                    <div class="cursor-pointer" :title="$t('2108')">
-                        <YoutubeIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
-                        <div class="h-6 mt-1 bg-transparent flex items-end justify-center">
-                            {{ playlist.course.total_video ? playlist.course.total_video : 0 }}
-                        </div>
-                    </div>
-                    <div class="cursor-pointer" :title="$t('pdf')">
-                        <PdfIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
-                        <div class="h-6 mt-1 bg-transparent flex items-end justify-center">
-                            {{ playlist.course.total_pdf ? playlist.course.total_pdf : 0 }}
-                        </div>
-                    </div>
-                    <div class="cursor-pointer" :title="$t('support')">
-                        <ChatIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
-                        <div class="h-6 mt-1 bg-transparent flex items-end justify-center"
-                             :class="darkMode?`text-skyBlue`:`text-primary`">
-                            {{ playlist.course.has_support ? $t('1008') : $t('1009') }}
-                        </div>
-                    </div>
-                    <div class="cursor-pointer text-center flex justify-center flex-col" :title="$t('2111')">
-                        <TestIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
-                        <div class="h-6 mt-1 bg-transparent"
-                             :class="darkMode?`text-skyBlue`:`text-primary`">
-                            {{ playlist.course.has_quiz ? $t('1008') : $t('1009') }}
-                        </div>
-                    </div>
-                    <div class="cursor-pointer" :title="$t('1114')">
-                        <CertificateIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
-                        <div class="h-6 mt-1 bg-transparent flex items-end justify-center"
-                             :class="darkMode?`text-skyBlue`:`text-primary`">
-                            {{ playlist.course.has_certificate ? $t('1008') : $t('1009') }}
-                        </div>
+        <div :class="darkMode ?`bg-youtube` : `bg-gray-50`" class="p-5 h-screen overflow-y-scroll">
+            <div class="w-9/12" :class="darkMode ?`bg-youtube text-textSecondary` : `bg-white shadow rounded-b-xl`">
+                <div class="relative">
+                    <img :src="detail.thumbnail" class="w-full m-auto">
+                    <div class="w-full absolute left-0 -bottom-5 flex justify-center">
+                        <button
+                                @click="gotToPlayList"
+                                class="flex items-center justify-center bg-primary text-white w-1/2 py-2 outline-none rounded-full shadow text-xl">
+                            <VideoCameraIcon fill="#fff" :size="35"/>
+                            <span class="ml-3">{{ $t('1117') }}</span>
+                        </button>
                     </div>
                 </div>
-            </div>
-
-            <div class="flex items-center justify-center my-10 pb-20" v-if="!loading">
-                <div class="progressVideoDetail flex items-center justify-center">
-                    <div>
-                        <div class="barOverflowVideoDetail">
-                            <div class="reportDetail"
-                                 :style="{transform:`rotate(${45+(Math.round(report('percentage'))*1.8)}deg)`}"></div>
+                <div class="my-10 px-5">
+                    <div class="font-bold text-2xl">{{ detail.teacher.name }} ({{ detail.title }})</div>
+                    <div class="flex justify-between items-center my-5">
+                        <div class="text-lg">
+                            <template v-if="detail.price.highlight">
+                                {{ $t('1006') }}:
+                                <span class="mr-3">
+                                    $ <del>{{ khmerNumber(detail.price.highlight) }}</del>
+                                </span>
+                                <span class="text-red-600 font-semibold">
+                                    $ {{ khmerNumber(detail.price.year) }}
+                                </span>
+                            </template>
+                            <template v-else>
+                                <span>{{ $t('1007') }}</span>
+                            </template>
                         </div>
-                        <div class="absolute w-full top-0 h-full flex flex-col justify-end items-center">
-                            <div class="text-xl">
-                                {{ $t('1117') }}
+
+                        <div class="flex items-center">
+                            <Eye :height="30"></Eye>
+                            <span v-if="loading">
+                                <Loader :size="10" bg="#000"/>
+                            </span>
+                            <span v-else>
+                                <span class="pl-2">
+                                    {{ kFormatter(playlist.course.view) }}
+                                </span>
+                            </span>
+                        </div>
+                        <div class="cursor-pointer">
+                            <div
+                                    v-if="playlist.is_favorite"
+                                    @click="removeFavorite">
+                                <FavoriteFill
+                                        fill="#c0272d"
+                                        :size="30"/>
                             </div>
-                            <div class="font-bold text-3xl mt-2">
-                                {{ report('watch') }}/{{ report('total') }}
+                            <div
+                                    v-else
+                                    @click="addToFavorite">
+                                <FavoriteIcon
+                                        :size="30"/>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="border-b border-t py-3 px-5" :class="darkMode ?`border-button` : ``" v-if="!loading">
+                    <div class="flex items-center text-center justify-between w-96">
+                        <div class="cursor-pointer" :title="$t('2108')">
+                            <YoutubeIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
+                            <div class="h-6 mt-1 bg-transparent flex items-end justify-center">
+                                {{ playlist.course.total_video ? playlist.course.total_video : 0 }}
+                            </div>
+                        </div>
+                        <div class="cursor-pointer" :title="$t('pdf')">
+                            <PdfIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
+                            <div class="h-6 mt-1 bg-transparent flex items-end justify-center">
+                                {{ playlist.course.total_pdf ? playlist.course.total_pdf : 0 }}
+                            </div>
+                        </div>
+                        <div class="cursor-pointer" :title="$t('support')">
+                            <ChatIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
+                            <div class="h-6 mt-1 bg-transparent flex items-end justify-center"
+                                 :class="darkMode?`text-skyBlue`:`text-primary`">
+                                {{ playlist.course.has_support ? $t('1008') : $t('1009') }}
+                            </div>
+                        </div>
+                        <div class="cursor-pointer text-center flex justify-center flex-col" :title="$t('2111')">
+                            <TestIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
+                            <div class="h-6 mt-1 bg-transparent"
+                                 :class="darkMode?`text-skyBlue`:`text-primary`">
+                                {{ playlist.course.has_quiz ? $t('1008') : $t('1009') }}
+                            </div>
+                        </div>
+                        <div class="cursor-pointer" :title="$t('1114')">
+                            <CertificateIcon :fill="darkMode?`#909090`:`#000000`" :size="42"/>
+                            <div class="h-6 mt-1 bg-transparent flex items-end justify-center"
+                                 :class="darkMode?`text-skyBlue`:`text-primary`">
+                                {{ playlist.course.has_certificate ? $t('1008') : $t('1009') }}
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <div class="flex items-center justify-center my-10 pb-20" v-if="!loading">
+                    <div class="progressVideoDetail flex items-center justify-center">
+                        <div>
+                            <div class="barOverflowVideoDetail">
+                                <div class="reportDetail"
+                                     :style="{transform:`rotate(${45+(Math.round(report('percentage'))*1.8)}deg)`}"></div>
+                            </div>
+                            <div class="absolute w-full top-0 h-full flex flex-col justify-end items-center">
+                                <div class="text-xl">
+                                    {{ $t('1117') }}
+                                </div>
+                                <div class="font-bold text-3xl mt-2">
+                                    {{ report('watch') }}/{{ report('total') }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div class="h-40"></div>
         </div>
-        <div class="h-40"></div>
     </div>
 </template>
 
@@ -136,9 +143,11 @@
     import PdfIcon from "@/components/PdfIcon";
     import ChatIcon from "@/components/ChatIcon";
     import YoutubeIcon from "@/components/YoutubeIcon";
+    import BackMenuIcon from "../../components/BackMenuIcon";
 
     export default {
         components: {
+            BackMenuIcon,
             YoutubeIcon,
             CertificateIcon,
             VideoCameraIcon,
