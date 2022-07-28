6efd783dd6f48ba5ce5c8863caaf98ccf13d72fe
CHANN VUTHY <channvvuthy@users.noreply.github.com>
2022-07-28 09:12:24 +0700

    Fix bug

--- src/views/Sidebar/components/eSchool.vue
+++ src/views/Sidebar/components/eSchool.vue
@@ -82,7 +82,7 @@
                         {{ darkMode ? $t('light_mode') : $t('1133') }}
                     </div>
                 </div>
-                <div class="text-center flex flex-col justify-center items-center">
+                <div class="text-center flex flex-col justify-center items-center khmer_os">
                     <div class="flex-col flex justify-center items-center  cursor-pointer h-13 w-13 rounded-full"
                          :class="activeClass('')"
                          @click="setLocalization(localize==='kh'?'en':'kh')">
